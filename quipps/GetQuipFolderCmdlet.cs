8f277553b1c70fed5492959218d64ef0bc66e351
Jérôme Boivin <boivin.jerome@gmail.com>
2018-09-26 12:53:50 +0200

    Added Get-QuipFolder cmdlet

---
+++ quipps/GetQuipFolderCmdlet.cs
@@ -0,0 +1,20 @@
+﻿using libquip.folders;
+using System.Management.Automation;
+
+namespace quipps
+{
+	[Cmdlet(VerbsCommon.Get, "QuipFolder")]
+	[OutputType(typeof(QuipFoldersResponse))]
+	public class GetQuipFolderCmdlet: QuipApiCmdlet
+	{
+		[Parameter(ValueFromPipelineByPropertyName = true, ValueFromPipeline = true)]
+		public string Id { get; set; }
+
+		protected override void ProcessRecord()
+		{
+			QuipFolder quipFolder = new QuipFolder(ApiKey);
+			var response = quipFolder.GetFolder(Id);
+			WriteObject(response);
+		}
+	}
+}
--- quipps/GetRecentThreadsCmdlet.cs
+++ quipps/GetRecentThreadsCmdlet.cs
@@ -8,10 +8,13 @@ namespace quipps
 	[OutputType(typeof(Dictionary<string, Document>))]
 	public class GetRecentThreadsCmdlet: QuipApiCmdlet
 	{
+		[Parameter(ValueFromPipelineByPropertyName = true)]
+		public int Count { get; set; }
+
 		protected override void ProcessRecord()
 		{
 			QuipThread quipThread = new QuipThread(ApiKey);
-			var response = quipThread.GetRecent();
+			var response = Count > 0 ? quipThread.GetRecent(Count) : quipThread.GetRecent();
 			WriteObject(response);
 		}
 	}
