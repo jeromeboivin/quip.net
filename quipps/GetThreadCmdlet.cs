4b888a8da6f9752f98b18d01751ccf2145c97cbe
Jerome Boivin <boivin.jerome@gmail.com>
2024-02-13 15:22:25 +0100

    Added Get-QuipThreadHtml CmdLet

--- quipps/GetThreadCmdlet.cs
+++ quipps/GetThreadCmdlet.cs
@@ -34,4 +34,35 @@ namespace quipps
 			WriteObject(response.thread);
 		}
 	}
+
+	[Cmdlet(VerbsCommon.Get, "QuipThreadHtml")]
+	[OutputType(typeof(Thread))]
+	public class GetThreadHtmlCmdlet : QuipApiCmdlet
+	{
+		[Parameter(Position = 0, ValueFromPipelineByPropertyName = true, ValueFromPipeline = true, ParameterSetName = "Id")]
+		public string Id { get; set; }
+
+		[Parameter(Position = 0, ValueFromPipelineByPropertyName = true, ValueFromPipeline = true, ParameterSetName = "Url")]
+		public Uri Url { get; set; }
+
+		protected override void ProcessRecord()
+		{
+			Uri threadUri;
+
+			if (Url != null)
+			{
+				// Remove trailing '/' to get thread id from url
+				Id = Url.PathAndQuery.Substring(1);
+			}
+			else if (Uri.TryCreate(Id, UriKind.Absolute, out threadUri))
+			{
+				// Remove trailing '/' to get thread id from url
+				Id = threadUri.PathAndQuery.Substring(1);
+			}
+
+			QuipThread quipThread = new QuipThread(ApiKey);
+			var response = quipThread.GetThread(Id);
+			WriteObject(response.html);
+		}
+	}
 }
